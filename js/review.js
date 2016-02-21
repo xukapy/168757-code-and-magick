b2cb5b3579da3522b97c539b158feb124d5fd45b
xukapy <s.xukapy@gmail.com>
2016-02-22 01:03:57 +0300

    Занятие 7 Задание 1

---
+++ js/review.js
@@ -0,0 +1,54 @@
+'use strict';
+
+(function() {
+
+  var starsClassName = [
+    'review-rating',
+    'review-rating',
+    'review-rating-two',
+    'review-rating-three',
+    'review-rating-four',
+    'review-rating-five'
+  ];
+
+  function Review(data) {
+    this._data = data;
+  }
+
+  Review.prototype.render = function() {
+    var TIMEOUT = 10000;
+    var IMG_WIDTH = 124;
+    var IMG_HEIGHT = 124;
+    var template = document.querySelector('#review-template');
+
+    this.element = template.content.children[0].cloneNode(true);
+    var imgElement = this.element.querySelector('img.review-author');
+    //Наполняем данными
+    this.element.querySelector('p.review-text').textContent = this._data.description;
+    this.element.querySelector('span.review-rating').classList.add(starsClassName[this._data.rating]);
+
+    //Все изображения создаёт с помощью new Image() и добавляет им обработчики загрузки и ошибки
+    var avatarImage = new Image(IMG_WIDTH, IMG_HEIGHT);
+    avatarImage.src = this._data.author.picture;
+    avatarImage.title = this._data.author.name;
+    avatarImage.classList.add('review-author');
+
+    var timer = setTimeout(function() {
+      avatarImage.src = ''; //Прекращаем загрузку
+      this.element.classList.add('review-load-failure'); //Обработчик ошибки
+    }.bind(this), TIMEOUT);
+
+    avatarImage.onerror = function() {
+      this.element.classList.add('review-load-failure'); //Обработчик ошибки
+    }.bind(this);
+
+    avatarImage.onload = function() {
+      clearTimeout(timer);
+      this.element.replaceChild(avatarImage, imgElement);
+    }.bind(this);
+
+  };
+
+  window.Review = Review;
+
+})();
--- js/reviews.js
+++ js/reviews.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* global Review: true */
+
 (function() {
   var reviews;
   var filteredReviews = [];
@@ -28,54 +30,14 @@
 
   var container = document.querySelector('div.reviews-list');
 
-  var starsClassName = [
-    'review-rating',
-    'review-rating',
-    'review-rating-two',
-    'review-rating-three',
-    'review-rating-four',
-    'review-rating-five'
-  ];
-
-  //Создаёт для каждой записи массива reviews блок отзыва на основе шаблона #review-template
-  function getElementFromTemplate(data) {
-    var TIMEOUT = 10000;
-    var IMG_WIDTH = 124;
-    var IMG_HEIGHT = 124;
-    var template = document.querySelector('#review-template');
-    var element = template.content.children[0].cloneNode(true);
-    var imgElement = element.querySelector('img.review-author');
-    //Наполняем данными
-    element.querySelector('p.review-text').textContent = data.description;
-    element.querySelector('span.review-rating').classList.add(starsClassName[data.rating]);
-
-    //Все изображения создаёт с помощью new Image() и добавляет им обработчики загрузки и ошибки
-    var avatarImage = new Image(IMG_WIDTH, IMG_HEIGHT);
-    avatarImage.src = data.author.picture;
-    avatarImage.title = data.author.name;
-    avatarImage.classList.add('review-author');
-    var timer = setTimeout(function() {
-      avatarImage.src = ''; //Прекращаем загрузку
-      element.classList.add('review-load-failure'); //Обработчик ошибки
-    }, TIMEOUT);
-
-    avatarImage.onerror = function() {
-      element.classList.add('review-load-failure'); //Обработчик ошибки
-    };
-
-    avatarImage.onload = function() {
-      clearTimeout(timer);
-      element.replaceChild(avatarImage, imgElement);
-    };
-
-    return element;
-  }
-
   getReviews();
 
   function renderReviews(reviewsToRender, pageNumber, clearContainer) {
     if (clearContainer) {
-      container.innerHTML = '';
+      var renderedElements = container.querySelectorAll('.review');
+      [].forEach.call(renderedElements, function(el) {
+        container.removeChild(el);
+      });
     }
     var from = pageNumber * PAGE_SIZE;
     var to = from + PAGE_SIZE;
@@ -84,8 +46,10 @@
     var fragment = document.createDocumentFragment();
 
     pageReviews.forEach(function(review) {
-      var element = getElementFromTemplate(review);
-      fragment.appendChild(element);
+      //var element = getElementFromTemplate(review);
+      var reviewElement = new Review(review);
+      reviewElement.render();
+      fragment.appendChild(reviewElement.element);
     });
 
     container.appendChild(fragment);
